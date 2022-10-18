f151b330e24659da739bde28c4bea34782a7122a
Jair Partida <jairpartida1@gmail.com>
2022-10-17 22:31:17 -0600

    creating the context

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,33 @@
 import { Container, Grid, Typography } from '@mui/material'
 import Form from './components/Form'
+import { NewsProvider } from './context/NewsProvider'
 
 function App() {
 
   return (
-    <Container>
-        <header>
-            <Typography
-              align='center' marginY={5} component='h1' variant='h3'
-            > 
-              News App 
-            </Typography>
-        </header>
+    <NewsProvider>
+      <Container>
+          <header>
+              <Typography
+                align='center' marginY={5} component='h1' variant='h3'
+              > 
+                News App 
+              </Typography>
+          </header>
 
-        <Grid
-          container
-          direction='row'
-          justifyContent='center'
-          alignItems='center'
-        >
-          <Grid item xs={12} sm={10} md={6} lg={4}>
-              <Form />
+          <Grid
+            container
+            direction='row'
+            justifyContent='center'
+            alignItems='center'
+          >
+            <Grid item xs={12} sm={10} md={6} lg={4}>
+                <Form />
+            </Grid>
           </Grid>
-        </Grid>
-        
-
-    </Container>
+          
+      </Container>
+    </NewsProvider>
   )
 }
 
